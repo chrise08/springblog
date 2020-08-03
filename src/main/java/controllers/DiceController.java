36aa79d94cb5f36cf9fa5c4c8fa0ee4411e36bb4
Chris Eckert <chris.eckert08@gmail.com>
2020-08-03 16:57:30 -0500

    spring views exercises

---
+++ src/main/java/controllers/DiceController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class DiceController {
+	@GetMapping("/dice-roll")
+	public String viewRollDice(){
+		return "dice-roll";
+	}
+	
+	@GetMapping("/dice-roll/{n}")
+	public String viewResults(@PathVariable int n, Model model){
+		
+		int diceRoll = (int) (Math.random() * 6) + 1;
+		String message;
+		
+		if(diceRoll == n){
+			message = "Great job! You guessed the roll number!";
+		}else{
+			message = "Sorry, not quite! Try again!";
+		}
+		
+		model.addAttribute("n", n);
+		model.addAttribute("message", message);
+		model.addAttribute("diceRoll", diceRoll);
+		
+		return "dice-results";
+	}
+}
--- src/main/java/controllers/PostController.java
+++ src/main/java/controllers/PostController.java
@@ -1,18 +1,27 @@
-package com.codeup.springblog;
+package controllers;
 
+import models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
 	
 	@GetMapping("/posts")
-	@ResponseBody
-	public String postsIndex() {
-		return "posts index page";
+	public String postsIndex(Model model) {
+		ArrayList<Post> posts = new ArrayList<>();
+		posts.add(new Post(2, "A Title", "A description"));
+		posts.add(new Post(3, "Another Title", "Another description"));
+		posts.add(new Post(4, "Yet Another Title", "Yet another description"));
+		
+		model.addAttribute("posts", posts);
+		return "posts/index";
 	}
 	
 	@GetMapping("/posts/{id}")
